6f74ac5e460ea504334fea804bfa57f1fd0d9c18
JonathanCamberos <jonathancamberos@gmail.com>
2024-08-05 17:33:28 -0600

    created server instance

---
+++ Server.py
@@ -0,0 +1,66 @@
+import socket
+import select
+import sys
+import argparse
+# import pymongo 
+import secrets
+import uuid
+import datetime
+
+# Hello! This is the main code for the Bank Server
+# This section of the Banking Application will be in charge of:
+#   - Starting communications from clients/users
+#       - Diffie-Hellman exchange --> Shared_secret
+#       - IV Generator            --> For Modes Encryption/Decryption
+
+#   - Taking client/users
+#       - Request CRUD (Create, Read, Update, Delete) to Backend Database
+#       - 
+
+#   - Verifying with Certificate Server (TO-DO)
+#       - 
+if __name__ == '__main__':
+
+    print("Hello!")
+
+     # 0.1 - Argument validation (ignore)
+    if len(sys.argv) < 1:
+        print("Usage: python3 ServerBank.py [--ip_port IP_PORT] ")
+        exit(1)
+    else:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--ip_port',type=int,required=False,help='The port that the BitTorrent clienct connects to')
+
+        args = parser.parse_args()
+
+        #print("Correct number of arguments")
+        if args.ip_port is not None:
+            print(f'Running Banking Server with arguments: {args.ip_port}')
+        else:
+            print("\nRunning Banking Server with default port '6969'")
+
+
+    # 1.1 - Server Information
+    server_ip = '0.0.0.0'  # Use '0.0.0.0' to listen on all available interfaces
+    default_port = 4000
+
+    # 1.2 - Creating Server Socket 
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    #1.3 Binding to Server Socket Object
+    if args.ip_port is not None:
+        server_socket.bind((server_ip, args.ip_port))
+    else:
+        server_socket.bind((server_ip, default_port))
+
+    #1.4 - Listen on that Server Socket (Port)
+    server_socket.listen()
+
+    print("We are a serverrrrr We only LISTENINGGGGGG USING OUR EARSSSSS ***************************")
+
+    #1.5 - Sanity Test Printing
+    if args.ip_port is not None:
+        print(f"Server listening on {server_ip}:{args.ip_port}\n")
+    else:
+        print(f"Server listening on {server_ip}:{default_port}\n")
